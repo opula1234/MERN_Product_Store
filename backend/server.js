f838e309dc7a8426af0ba0ab3ae2e9e105aaa01c
opula1234 <balajioropula@gmail.com>
2025-08-16 04:07:58 +0530

    Feature: Added Mongodb connection script

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+dotenv.config();
+
+// console.log(process.env.MONGO_URI);
 
 const app = express();
 
-app.get("/",(req, res) => {
-    res.send("Hello world!")
+
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server is running on port 3000 Hello world! learning mern stack");
+});
+
+app.get("/products",(req, res) => {
+    res.send("Server is ready and its running on port 5000");
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000 Hello world!");
-});
+
