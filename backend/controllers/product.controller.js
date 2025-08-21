b8786521cf89961f83bb145ced41adfb4a3d4a82
opula1234 <balajioropula@gmail.com>
2025-08-21 16:34:33 +0530

    Feature: Modified codes for a good project structure

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,25 +1,64 @@
 import mongoose from "mongoose";
 import Product from "../models/product.model.js"
 
-export const getProducts = async (res, req) => {
-    try{
+export const getProducts = async (req, res) => {
+    try {
         const products = await Product.find({});
-        res.status(200).json({success: true, data: products})
+        res.status(200).json({ success: true, data: products })
     }
-    catch (error){
-        console.log("❌ Error in fetching products", error.message)
-        res.status(500).json({ success: false, message: "❌ Server Error"})
+    catch (error) {
+        console.log(" ⛔ Error in fetching products", error.message)
+        res.status(500).json({ success: false, message: "Server Error" })
     }
 }
 
 
-export const createProduct = async (res, req) => {
-    try{
-        const product = req.body
+export const createProduct = async (req, res) => {
+    const product = req.body
 
-        if (!prod)
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all the details..." })
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct })
     }
     catch (error) {
+        console.log(" ⛔ Error in new Product creation", error.message)
+        res.status(500).json({ success: false, message: error.message })
+    }
+}
 
+export const updateProduct = async (req, res) => {
+    const { id } = req.params
+
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid Product id" })
+    }
+
+    try {
+        const updated_product = await Product.findByIdAndUpdate(id, product, { new: true })
+        res.status(200).json({ success: true, data: updated_product })
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server error" })
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params
+    console.log("id:", id)
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted successfully" });
+    }
+    catch (error) {
+        console.log("Error in fetching product", error.message)
+        res.status(404).json({ success: false, message: "Product not found" });
     }
 }
