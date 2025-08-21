68e05cee28ca7e23da4bd31bec904fca14ecca7a
opula1234 <balajioropula@gmail.com>
2025-08-21 16:46:39 +0530

    Fix: code changes in server.js file added port details that coming up from environment variables.

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,15 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("💻 ⚙️   Server is running on port  #️⃣  5000 Hello world! learning mern stack");
+    console.log("Server started at http://localhost:" + PORT);
 });
 
 
